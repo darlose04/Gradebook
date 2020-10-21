281c2ae568c92d4cc6cce22126cf6e84a8f31122
Zach Smith <zsmith126028@gmail.com>
2020-10-21 13:19:54 -0500

    add ShowStatistics method

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -13,6 +14,23 @@ namespace GradeBook
         public void AddGrade(double grade)
         {
             grades.Add(grade);
+        }
+
+        public void ShowStatistics() {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            
+            foreach(double number in grades) {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
+            }
+
+            double avg = result / grades.Count;
+            Console.WriteLine($"The average grade is {avg:N2}");
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
         }
 
         private List<double> grades;
