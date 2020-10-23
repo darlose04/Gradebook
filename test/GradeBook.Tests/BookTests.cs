5569982a780795504046e20cfc240cc0c29fce5c
Zach Smith <zsmith126028@gmail.com>
2020-10-23 13:14:50 -0500

    add decimal point precision to unit test

--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -28,17 +28,17 @@ namespace GradeBook.Tests
         {
             // arrange
             var book = new Book("");
-            book.Add(89.1);
-            book.Add(90.5);
-            book.Add(77.3);
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
 
             //act
             var result = book.GetStatistics();
 
             // assert
-            Assert.Equal(85.6, result.Average);
-            Assert.Equal(90.5, result.High);
-            Assert.Equal(77.3, result.Low);
+            Assert.Equal(85.6, result.Average, 1);
+            Assert.Equal(90.5, result.High, 1);
+            Assert.Equal(77.3, result.Low, 1);
         }
     }
 }
