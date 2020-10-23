06a4272b33c52668df9faf2f4ead60753a18f55a
Zach Smith <zsmith126028@gmail.com>
2020-10-23 12:53:37 -0500

    change name of file and test class. add reference to class

---
+++ test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,38 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    // public class UnitTest1
+    // {
+    //     [Fact]
+    //     public void Test1()
+    //     {
+    //         // arrange - put together test data
+    //         var x = 5;
+    //         var y = 2;
+    //         var expected = 7;
+
+    //         // act - perform calculation
+    //         var actual = x + y;
+
+    //         // assert section
+    //         Assert.Equal(expected, actual);
+    //     }
+    // }
+    
+    public class BookTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            // arrange
+            var book = new Book("");
+
+            //act
+            
+
+            // assert
+        }
+    }
+}
