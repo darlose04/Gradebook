79917945012c26669a88cd246272aa149f4339b7
Zach Smith <zsmith126028@gmail.com>
2020-10-23 13:14:59 -0500

    fix typo

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -12,7 +12,7 @@ namespace GradeBook
             var book = new Book("Scott's Grade Book");
             book.AddGrade(89.1);
             book.AddGrade(90.5);
-            book.AddGrade(77.5);\
+            book.AddGrade(77.5);
 
             var stats = book.GetStatistics();
             
