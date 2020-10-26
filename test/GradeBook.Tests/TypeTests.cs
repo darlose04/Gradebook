6226e74d9f8cfd60458daa19b561544dea1a12e8
Zach Smith <zsmith126028@gmail.com>
2020-10-26 11:46:54 -0500

    adding tests to check references and values

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -9,10 +9,35 @@ namespace GradeBook.Tests
         public void Test1()
         {
             var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void SetName(Book book1, string name)
+        {
+            book1.Name = name;
+        }
+
+        [Fact]
+        public void GetBookReturnsDifferentObjects()
+        {
+            var book1 = GetBook("Book 1");
             var book2 = GetBook("Book 2");
 
             Assert.Equal("Book 1", book1.Name);
             Assert.Equal("Book 2", book2.Name);
+            Assert.NotSame(book1, book2);
+        }
+
+        [Fact]
+        public void TwoVarsCanReferenceSameObject()
+        {
+            var book1 = GetBook("Book 1");
+            var book2 = book1;
+
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
         }
 
         Book GetBook(string name)
