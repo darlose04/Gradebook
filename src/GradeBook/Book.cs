eff42faa89e5f261808348aef58d9d4bbd4a99be
Zach Smith <zsmith126028@gmail.com>
2020-10-27 12:08:06 -0500

    trying switch statement

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -11,6 +11,27 @@ namespace GradeBook
             this.Name = name; // could also get rid of this keyword
         }
 
+        public void AddLetterGrade(char letter) {
+            switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                case 'D':
+                    AddGrade(60);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+
         public void AddGrade(double grade)
         {
             if (grade <= 100 && grade >= 0) {
@@ -34,6 +55,11 @@ namespace GradeBook
             // }
 
             for(int index = 0; index < grades.Count; index++) {
+                if (grades[index] == 42.1) {
+                    // break;
+                    // continue;
+                }
+
                 result.Low = Math.Min(grades[index], result.Low);
                 result.High = Math.Max(grades[index], result.High);
                 result.Average += grades[index];
