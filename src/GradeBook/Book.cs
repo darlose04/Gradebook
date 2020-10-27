b51fe7af4d69370c96edff33cbc559591a5a73fb
Zach Smith <zsmith126028@gmail.com>
2020-10-27 12:26:13 -0500

    getting user inputs, throwing exceptions

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -37,7 +37,7 @@ namespace GradeBook
             if (grade <= 100 && grade >= 0) {
                 grades.Add(grade);
             } else {
-                Console.WriteLine("Invalid value");
+                throw new ArgumentException($"Invalid {nameof(grade)}");
             }
             
         }
@@ -48,22 +48,22 @@ namespace GradeBook
             result.High = double.MinValue;
             result.Low = double.MaxValue;
             
-            // foreach(double grade in grades) {
-            //     result.Low = Math.Min(grade, result.Low);
-            //     result.High = Math.Max(grade, result.High);
-            //     result.Average += grade;
-            // }
-
-            for(int index = 0; index < grades.Count; index++) {
-                if (grades[index] == 42.1) {
-                    // break;
-                    // continue;
-                }
-
-                result.Low = Math.Min(grades[index], result.Low);
-                result.High = Math.Max(grades[index], result.High);
-                result.Average += grades[index];
+            foreach(double grade in grades) {
+                result.Low = Math.Min(grade, result.Low);
+                result.High = Math.Max(grade, result.High);
+                result.Average += grade;
             }
+
+            // for(int index = 0; index < grades.Count; index++) {
+            //     if (grades[index] == 42.1) {
+            //         // break;
+            //         // continue;
+            //     }
+
+            //     result.Low = Math.Min(grades[index], result.Low);
+            //     result.High = Math.Max(grades[index], result.High);
+            //     result.Average += grades[index];
+            // }
 
             // var index = 0;
 
@@ -86,6 +86,25 @@ namespace GradeBook
             // } while (index < grades.Count);
 
             result.Average /= grades.Count;
+
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
 
             return result;
         }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,15 +10,40 @@ namespace GradeBook
             // double[] numbers = new double[] { 12.7, 10.3, 6.11, 4.1};
 
             var book = new Book("Scott's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+            // book.AddGrade(89.1);
+            // book.AddGrade(90.5);
+            // book.AddGrade(77.5);
+
+            // var done = false;
+
+            // while (!done) 
+            while (true) 
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+
+                var input = Console.ReadLine();
+
+                if (input == "q") {
+                    // done = true;
+                    // continue;
+
+                    break;
+                }
+
+                var grade = double.Parse(input);
+                book.AddGrade(grade);
+            }
+
+            
+
+            
 
             var stats = book.GetStatistics();
             
             Console.WriteLine($"The average grade is {stats.Average:N2}");
             Console.WriteLine($"The lowest grade is {stats.Low}");
             Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
         }
     }
 }
